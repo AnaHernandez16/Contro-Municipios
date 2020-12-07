58271b0bd48e2409a89a83ca649cfe90223f8afc
Omar Puc Ek <65086436+Omar-Puc@users.noreply.github.com>
2020-12-07 17:55:46 -0500

    Mi parte
    -Creación del proyecto
    -Creación del IndexController
    -Creación del Navbar: Navbar.html
    -Creación de la vista principal del proyecto: Home.html

---
+++ VDAR/src/main/java/com/example/demo/controllers/IndexController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/app")
+public class IndexController {
+	
+
+	@GetMapping({"/","","/home"})
+	public String index(Model model) {
+		
+		model.addAttribute("titulo", "Control municipios");
+		
+		return "home";
+	}
+	
+
+	
+}
