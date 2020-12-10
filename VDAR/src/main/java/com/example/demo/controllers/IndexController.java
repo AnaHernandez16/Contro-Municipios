2742171278320bca1b79d7459c7acfdb9016ba10
AnaHernandez16 <64914606+AnaHernandez16@users.noreply.github.com>
2020-12-10 09:34:00 -0500

    Update IndexController.java

--- VDAR/src/main/java/com/example/demo/controllers/IndexController.java
+++ VDAR/src/main/java/com/example/demo/controllers/IndexController.java
@@ -2,10 +2,27 @@ package com.example.demo.controllers;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.entity.Casa;
+
+import com.example.demo.service.CasasService;
+
+
+
 
 
 @Controller
@@ -23,7 +40,7 @@ public class IndexController {
 	
 
 	@GetMapping({"/formulario_consulta"})
-	public String Menu(Model model) {
+	public String segundex(Model model) {
 		
 		model.addAttribute("titulo", "Control municipios");
 		
@@ -37,5 +54,25 @@ public class IndexController {
 		
 		return "formulario_consulta2";
 	}
+	
+	@Autowired
+	@Qualifier("casasservice")
+	private CasasService casasService;
+	
+	@GetMapping("/formulario_casas")
+	public ModelAndView formCasas(Model model) {
+		
+		model.addAttribute("titulo", "Formulario casas");
+		ModelAndView mav = new ModelAndView("formulario_casas");
+		mav.addObject("casa", new Casa());
+		return mav;
+	}
+	
+	@PostMapping("/agregarCasa")
+	public String addUsers(@ModelAttribute(name="casa") Casa casa) {
+		casasService.addCasa(casa);
+		return "redirect:/app/formulario_casas";
+	}
+	
 	
 }
