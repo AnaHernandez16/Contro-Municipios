9ccec3a8b5ecf45cf9f400185ce2f7af10cc3286
Osmar <64910022+Osmar-Cima@users.noreply.github.com>
2020-12-11 10:24:13 -0500

    Add files via upload

---
+++ VDAR/src/main/java/com/example/demo/repository/MunicipioCrud.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.entity.Municipio;
+
+public interface MunicipioCrud extends CrudRepository<Municipio, Long>{
+
+}
