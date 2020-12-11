6cb39aa421c2daa2c625411c2ac5fee8cbd18824
AlondraChan <64912889+AlondraChan@users.noreply.github.com>
2020-12-10 20:08:13 -0500

    Add files via upload

---
+++ VDAR/src/main/java/com/example/demo/service/MunicipiosService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Municipio;
+
+public interface MunicipiosService {
+
+	public abstract Municipio addMunicipio(Municipio municipio);
+}
